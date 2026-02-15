e42c6c772800152e89ac9646b3465a522c71c3ed
Paul <p.evans@live.com>
2026-02-15 16:24:43 +0000

    Remove marketing app & move to /src/

--- apps/web-app/src/lib/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-  appUrl: process.env.NEXT_PUBLIC_APP_URL || "https://app.smashscheduler.com",
-  marketingUrl: process.env.NEXT_PUBLIC_MARKETING_URL || "https://smashscheduler.com",
-} as const;
